0cb9a502f5e716f5d50702273cffc61ef4217497
Raphael Fleury <raphafleury123@gmail.com>
2022-05-08 15:46:31 -0300

    feat: Prison class

--- BancoImobiliario/src/model/Prison.java
+++ BancoImobiliario/src/model/Prison.java
@@ -6,4 +6,30 @@ public class Prison {
 
     private ArrayList<Player> playerPrison = new ArrayList<>();
     
+    public boolean checkPlayerPrison(Player player) {
+    	
+    	for(int i = 0; i < playerPrison.size(); i++) {
+    		if(playerPrison.get(i) == player) {
+    			return true;
+    		} 
+    	}
+		return false;
+    }
+    
+    public void removeFromPrison(Player player) {
+    	if(checkPlayerPrison(player)) {
+    		playerPrison.remove(player);
+    	}
+    }
+    
+    public void addInPrison(Player player) {
+    	if(checkPlayerPrison(player)) {
+    		playerPrison.add(player);
+    	}
+    	
+    }
+    
+    public ArrayList<Player> getPrison() {
+    	return playerPrison;
+    }
 }
