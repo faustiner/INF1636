d8783cfa28a9043b485788e753adb7fc456962a4
Raphael Fleury <raphael.fleury@b2wdigital.com>
2022-06-01 03:43:58 -0300

    fix: code refactor

--- BancoImobiliario/src/main/Main.java
+++ BancoImobiliario/src/main/Main.java
@@ -7,7 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 		StartMenu inicial = new StartMenu();
-		 			// TODO Auto-generated method stub
 		 inicial.setVisible(true);	 
 	}
 }
--- BancoImobiliario/src/tests/GameManagerTest.java
+++ BancoImobiliario/src/tests/GameManagerTest.java
@@ -1,11 +1,7 @@
 package tests;
 
 import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-
 import model.GameManager;
-import model.Player;
 import org.junit.Test;
 
 public class GameManagerTest {
